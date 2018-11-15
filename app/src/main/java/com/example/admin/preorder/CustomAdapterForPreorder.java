8eea7fbab281c63ec5084bd8ddb15d209049fa91
athkrit <athkrit_tae@hotmail.com>
2018-11-16 01:00:01 +0700

    bug

--- app/src/main/java/com/example/admin/preorder/CustomAdapterForPreorder.java
+++ app/src/main/java/com/example/admin/preorder/CustomAdapterForPreorder.java
@@ -13,14 +13,26 @@ import java.util.ArrayList;
 
 public class CustomAdapterForPreorder extends BaseAdapter {
     Context mContext;
-    ArrayList<String> fruitname;
-    public CustomAdapterForPreorder(Context context, ArrayList<String> fruitname){
-        this.fruitname = fruitname;
+    ArrayList<String> clubkey = new ArrayList<String>();
+    ArrayList<Integer> countDurianA = new ArrayList<Integer>();
+    ArrayList<Integer> countLonganA = new ArrayList<Integer>();
+    ArrayList<Integer> countOrangeA = new ArrayList<Integer>();
+    ArrayList<Integer> countPineappleA = new ArrayList<Integer>();
+    ArrayList<Integer> countrambutanA = new ArrayList<Integer>();
+    ArrayList<Integer> countmangosteenA = new ArrayList<Integer>();
+    public CustomAdapterForPreorder(Context context, ArrayList<String> clubkey,ArrayList<Integer> countDurianA, ArrayList<Integer> countLonganA,ArrayList<Integer> countOrangeA,ArrayList<Integer> countPineappleA,ArrayList<Integer> countrambutanA,ArrayList<Integer> countmangosteenA){
         this.mContext = context;
+        this.clubkey = clubkey;
+        this.countDurianA =countDurianA;
+        this.countLonganA = countLonganA;
+        this.countmangosteenA = countmangosteenA;
+        this.countOrangeA = countOrangeA;
+        this.countPineappleA = countPineappleA;
+        this.countrambutanA = countrambutanA;
     }
     @Override
     public int getCount() {
-        return fruitname.size();
+        return clubkey.size();
     }
 
     @Override
@@ -38,10 +50,36 @@ public class CustomAdapterForPreorder extends BaseAdapter {
         View view;
         LayoutInflater mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = mInflater.inflate(R.layout.listday, parent, false);
-        final ImageView imageView = (ImageView)view.findViewById(R.id.img1);
-        imageView.setImageResource(R.drawable.mangosteen);
-        TextView text = (TextView)view.findViewById(R.id.text1);
-        text.setText(fruitname.get(position));
+        final TextView date = (TextView)view.findViewById(R.id.text1);
+        date.setText(""+clubkey.get(position));
+
+        ArrayList<String> name = new ArrayList<String>();
+
+        if(countDurianA.get(position)!=0){
+            ImageView img1 = view.findViewById(R.id.img1);
+            img1.setImageResource(R.drawable.durian);
+        }
+        if(countLonganA.get(position)!=0){
+            ImageView img2 = view.findViewById(R.id.img2);
+            img2.setImageResource(R.drawable.longan);
+        }
+        if(countrambutanA.get(position)!=0){
+            ImageView img3 = view.findViewById(R.id.img3);
+            img3.setImageResource(R.drawable.rambutan);
+        }
+        if(countOrangeA.get(position)!=0){
+            ImageView img4 = view.findViewById(R.id.img11);
+            img4.setImageResource(R.drawable.orange);
+        }
+        if(countmangosteenA.get(position)!=0){
+            ImageView img5 = view.findViewById(R.id.img12);
+            img5.setImageResource(R.drawable.mangosteen);
+        }
+        if(countPineappleA.get(position)!=0){
+            ImageView img6 = view.findViewById(R.id.img13);
+            img6.setImageResource(R.drawable.pineapple);
+        }
+
     return view;
     }
 }
--- app/src/main/java/com/example/admin/preorder/MainActivity.java
+++ app/src/main/java/com/example/admin/preorder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.preorder;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,59 +13,119 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    public DatabaseReference callPre;
+    public DatabaseReference callPre, callall;
     ArrayList<String> fruitID = new ArrayList<String>();
     ArrayList<String> fruitName = new ArrayList<String>();
-    ArrayList<String> price = new ArrayList<String>();
+    ArrayList<Integer> price = new ArrayList<Integer>();
     ArrayList<String> productName = new ArrayList<String>();
-    ArrayList<String> quantity = new ArrayList<String>();
+    ArrayList<Integer> quantity = new ArrayList<Integer>();
     ArrayList<String> unitPro = new ArrayList<String>();
     CustomAdapterForPreorder adapter;
+    ArrayList<String> clubkey = new ArrayList<String>();
+    String getK;
+    int countKey, countDurian, countLongan, countOrange, countPineapple, countrambutan, countmangosteen;
+    ArrayList<Integer> countDurianA = new ArrayList<Integer>();
+    ArrayList<Integer> countLonganA = new ArrayList<Integer>();
+    ArrayList<Integer> countOrangeA = new ArrayList<Integer>();
+    ArrayList<Integer> countPineappleA = new ArrayList<Integer>();
+    ArrayList<Integer> countrambutanA = new ArrayList<Integer>();
+    ArrayList<Integer> countmangosteenA = new ArrayList<Integer>();
+    ArrayList<String> fruitcount = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listday);
 
-        int[] fruit = {R.drawable.mangosteen, R.drawable.longan, R.drawable.orange, R.drawable.pineapple, R.drawable.rambutan, R.drawable.durian};
+
+        final int[] fruit = {R.drawable.mangosteen, R.drawable.longan, R.drawable.orange, R.drawable.pineapple, R.drawable.rambutan, R.drawable.durian};
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         callPre = database.getReference().child("product").child("preorderProduct");
         callPre.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Map map = (Map) dataSnapshot.getValue();
-//                String data = String.valueOf(map.get("-LR5McbfvCY5cZgZNVwf"));
-                fruitName.clear();
+                countKey = 0;
+                clubkey.clear();
+                countDurianA.clear();
+                countLonganA.clear();
+                countOrangeA.clear();
+                countPineappleA.clear();
+                countrambutanA.clear();
+                countmangosteenA.clear();
                 for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    getK = d.getKey();
+                    clubkey.add(getK);
+                    countKey = countKey + 1;
+                }
+//                for (int i = 0; i < countKey; i++) {
+//                    countDurian = 0;
+//                    countrambutan =0;
+//                    countPineapple =0;
+//                    countOrange = 0;
+//                    countmangosteen =0;
+//                    countLongan=0;
+                    callall = database.getReference().child("product").child("preorderProduct").child(clubkey.get(1));
+                    callall.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot d : dataSnapshot.getChildren()) {
+                                GetData get = d.getValue(GetData.class);
+                                fruitcount.add(get.getFruitName());
+                            }
+                            for (int x = 0; x < fruitcount.size(); x++) {
+                                if (fruitcount.get(x).equals("ทุเรียน")) {
+                                    countDurian++;
+                                }
+                                else if (fruitcount.get(x).equals("มังคุด")) {
+                                    countmangosteen++;
+                                }
+                                else if (fruitcount.get(x).equals("ลำไย")) {
+                                    countLongan++;
+                                }
+                                else if (fruitcount.get(x).equals("เงาะ")) {
+                                    countrambutan++;
+                                }
+                                else if (fruitcount.get(x).equals("แตงโม")) {
+                                    countPineapple++;
+                                }
+                                else {
+                                    countOrange++;
+                                }
+                            }
+                            countDurianA.add(countDurian);
+                            countLonganA.add(countLongan);
+                            countOrangeA.add(countOrange);
+                            countPineappleA.add(countPineapple);
+                            countrambutanA.add(countrambutan);
+                            countmangosteenA.add(countmangosteen);
 
-                    GetData m = d.getValue(GetData.class);
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-//                    fruitID.add(m.getFruitID());
-                    fruitName.add(m.getFruitName());
-//                    price.add(P.getPrice());
-//                    productName.add(P.getProductName());
-//                    quantity.add(P.getQuantity());
-                    //unitPro.add(P.getUnitPro());
+                        }
+                    });
+                    int coun = fruitcount.size();
+                    TextView textView =findViewById(R.id.text1);
+                textView.setText(""+countDurianA.size());
 
                 }
-                ListView list = (ListView) findViewById(R.id.list);
-                adapter = new CustomAdapterForPreorder(getApplicationContext(), fruitName);
-                list.setAdapter(adapter);
-//                list.setStackFromBottom(true);
-//                TextView text = findViewById(R.id.text3);
-//                text.setText(data);
-            }
+//                ListView list = (ListView) findViewById(R.id.list);
+//                adapter = new CustomAdapterForPreorder(getApplicationContext(),fruitcount);
+//                adapter = new CustomAdapterForPreorder(getApplicationContext(), clubkey, countDurianA, countLonganA, countOrangeA, countPineappleA, countrambutanA, countmangosteenA);
+//                list.setAdapter(adapter);
+//            }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                TextView text = findViewById(R.id.text3);
-                text.setText("data not found");
             }
         });
     }
